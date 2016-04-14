df59960b9857218d7695100baa3ac0da09d32e51
Bogdan Solomykin <bogdan@Bogdans-MacBook-Pro.local>
2016-04-14 20:41:11 +0300

    * test

--- inheritance/index.php
+++ inheritance/index.php
@@ -1,15 +1,7 @@
 <?php
 require_once 'man.php';
-
-
-
 require_once 'woman.php';
 
-$man =  new Man();
-$man  ->speak();
-
+$man = new Man();
+$man->speak();
 echo '<br/> ';
-
-$woman = new Woman();
-$woman->speak();
-$woman->age = 21;
