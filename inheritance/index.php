5fd894e53b08967ba0bc122b10c3dcb03c1e6876
Bogdan Solomykin <bogdan@Bogdans-MacBook-Pro.local>
2016-04-14 20:33:46 +0300

    * incapsulation explanation

--- inheritance/index.php
+++ inheritance/index.php
@@ -1,11 +1,14 @@
 <?php
 require_once 'man.php';
+
+
+
 require_once 'woman.php';
 
-$man = new Man();
-$man->speak();
+$man =  new Man();
+$man  ->speak();
 
-echo '<br />';
+echo '<br/> ';
 
 $woman = new Woman();
 $woman->speak();
