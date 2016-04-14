fbc86a3a480aa9512411fc1e6ff85bb151d64e65
Bogdan Solomykin <bogdan@Bogdans-MacBook-Pro.local>
2016-04-14 21:12:05 +0300

    + shoppingcart

---
+++ example/ShoppingCart.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bogdan
+ * Date: 4/14/16
+ * Time: 8:50 PM
+ */
+abstract class ShoppingCart
+{
+    abstract public function addToCart($productId);
+}
