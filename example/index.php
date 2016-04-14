14afdbdbcf849b06e37849d9e9edcaab4f558857
Bogdan Solomykin <bogdan@Bogdans-MacBook-Pro.local>
2016-04-14 21:15:28 +0300

    test

--- example/index.php
+++ example/index.php
@@ -4,10 +4,8 @@ require_once 'CustomCart.php';
 require_once 'MiniCart.php';
 
 $custom = new CustomCart();
-$productId = $_GET['product_id'];
-//$productId = 12;
+$productId = 12;
 $custom->addToCart($productId);
-redirect('homepage');
 
 $mini = new MiniCart();
 $mini->addToCart($productId);
